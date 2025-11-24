e4a0a2049bebd7c48c083edc885936476e42a8e9
Vx-tech <xaviervianney8@gmail.com>
2025-11-24 12:19:42 +0300

    Tracing errros

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,19 +1,18 @@
-import { exportPages } from "next/dist/export/worker"
 import { createContext, useContext, useState, ReactNode } from "react"
 
-interface CounterContextProps {
+interface CountContextProps {
     count: number;
     increment: () => void;
     decrement: () => void;
 }
 
-export const CountContext = createContext<CounterContextProps | undefined>
-(undefined)
+export const CountContext = createContext<CountContextProps | undefined>(undefined);
+
 
 export const CountProvider = ({ children }:{ children: ReactNode}) => {
     const [count, setCount] = useState<number>(0)
     const increment = () => setCount((count ) => count + 1)
-    const decrement = () => setCount((count) => count > 0 ? count <- 1 : 0)
+    const decrement = () => setCount((count) => (count > 0 ? count - 1 : 0));
 
     return (
         <CountContext.Provider value={{ count, increment, decrement }}>
